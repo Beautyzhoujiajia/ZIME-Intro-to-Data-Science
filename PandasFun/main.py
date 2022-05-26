1724502c35421efb94a3ca9add04f09847bfbe5e
Gina Sprint <sprint@gonzaga.edu>
2022-05-25 19:21:25 -0700

    finish write

--- PandasFun/main.py
+++ PandasFun/main.py
@@ -50,3 +50,52 @@ print(pop_ser, len(pop_ser), pop_ser.shape)
 pop_ser2 = pd.Series(dtype=float)
 pop_ser2["Liuzhou"] = 2.1
 print(pop_ser2)
+
+# let's start DataFrames
+# we can make a DataFrame from a 2D list
+# let's have a pop_df w/3 columns
+# 1 for City
+# 1 for Population
+# 1 for Class (small, medium, or large city)
+# task: make a 2D list to store this tabular data
+pop_data = [
+    ["Shanghai", 24.8, "Large"],
+    ["Beijing", 21.1, "Large"],
+    ["Hangzhou", 7.9, "Medium"],
+    ["Zibo", 2.6, "Small"]
+]
+header = ["City", "Population", "Class"]
+pop_df = pd.DataFrame(pop_data, columns=header)
+pop_df = pop_df.set_index("City")
+print(pop_df) # nicely pretty prints!!
+
+# indexing/slicing
+# grab a column by its label (to get a Series)
+pop_ser3 = pop_df["Population"]
+print(pop_ser3)
+# grab a column by its position (to get a Series)
+print(pop_df.iloc[:, 0])
+# grab a single value
+print(pop_df["Population"]["Shanghai"])
+print(pop_df.iloc[0, 0])
+# use .loc[] to mix label and position based indexing
+print(pop_df.loc["Shanghai", "Population"])
+
+# let's do a join demo!!
+# let's create a file called regions.csv
+# and we will open this file with pandas
+# to store its data in a DataFrame
+regions_df = pd.read_csv("regions.csv", index_col=0)
+print(regions_df)
+# then we will join it with pop_df
+merged_df = pop_df.merge(regions_df, on=["City"], how="outer")
+print(merged_df)
+
+# groupby demo
+# let's say we want to compute the average population by Class
+grouped_by_class = merged_df.groupby("Class")
+print(grouped_by_class["Population"].mean())
+
+# to wrap up...
+# let's save our merged_df to a file
+merged_df.to_csv("merged.csv")

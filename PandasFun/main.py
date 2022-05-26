348178be0f0eba87d5c65893474bbbaa4785a897
Gina Sprint <sprint@gonzaga.edu>
2022-05-25 18:31:07 -0700

    start of pandasfun

---
+++ PandasFun/main.py
@@ -0,0 +1,14 @@
+import pandas as pd # by convention
+
+# pandas is short for "panel data"
+# it is built on top of numpy (is short for "numerical python")
+# 2 main reasons to use pandas for data science
+# 1. label-based indexing (in addition to position-based indexing)
+# 2. really great builtin functionality for indexing, cleaning,
+# computing statistics, etc.
+
+# there are 2 main objects in pandas
+# 1. Series: 1D data
+# 2. DataFrame: 2D data
+
+# let start with Series
